f298f0f50514293072eef5778bf43b7cb7a06755
Jared O'Neal <joneal@anl.gov>
2024-01-16 10:42:08 -0600

    Have packages' test functions return success of test suite run.
    This could be useful for integrating calls to test() in CI build servers as
    well as general test scripts.

--- mytemplate_pypkg/src/mytemplate/test.py
+++ mytemplate_pypkg/src/mytemplate/test.py
@@ -14,7 +14,11 @@ def test(verbosity=1):
 
     Parameters
         verbosity - the verbosity level to pass to the unittest TestRunner
+    Return
+        True if all tests in package passed; False, otherwise.
     """
     loader = unittest.TestLoader()
     suite = load_tests(loader, None, None)
-    unittest.TextTestRunner(verbosity=verbosity).run(suite)
+    result = unittest.TextTestRunner(verbosity=verbosity).run(suite)
+
+    return result.wasSuccessful()
